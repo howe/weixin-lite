1ddadcefb87ac4e34e8929b330ec34c8e183d96f
Howe Chiang <howechiang@gmail.com>
2018-03-23 22:04:56 +0800

    .

--- src/main/java/org/nutz/weixin/util/Util.java
+++ src/main/java/org/nutz/weixin/util/Util.java
@@ -340,7 +340,7 @@ public class Util {
             }
             // 初始化
             Security.addProvider(new BouncyCastleProvider());
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7Padding", "BC");
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7Padding");
             SecretKeySpec spec = new SecretKeySpec(keyByte, "AES");
             AlgorithmParameters parameters = AlgorithmParameters.getInstance("AES");
             parameters.init(new IvParameterSpec(ivByte));
