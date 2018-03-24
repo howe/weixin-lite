8b1f5a5f524937353a9fb5b8acdf4c96a0fc148d
Howe Chiang <howechiang@gmail.com>
2018-03-25 00:10:19 +0800

    把方法清理了一遍，垃圾微信支付啊，风格一点都不统一

---
+++ src/main/java/org/nutz/weixin/bean/pay/notify/WxPayNotify.java
@@ -0,0 +1,353 @@
+package org.nutz.weixin.bean.pay.notify;
+
+/**
+ * Created on 2018/3/25
+ *
+ * @author Jianghao(howechiang @ gmail.com)
+ */
+public class WxPayNotify {
+
+    /**
+     * 返回状态码
+     */
+    private String return_code;
+
+    public String getReturn_code() {
+        return return_code;
+    }
+
+    public void setReturn_code(String return_code) {
+        this.return_code = return_code;
+    }
+
+    /**
+     * 返回信息
+     */
+    private String return_msg;
+
+    public String getReturn_msg() {
+        return return_msg;
+    }
+
+    public void setReturn_msg(String return_msg) {
+        this.return_msg = return_msg;
+    }
+
+    /**
+     * 应用ID
+     */
+    private String appid;
+
+    public String getAppid() {
+        return appid;
+    }
+
+    public void setAppid(String appid) {
+        this.appid = appid;
+    }
+
+    /**
+     * 商户号
+     */
+    private String mch_id;
+
+    public String getMch_id() {
+        return mch_id;
+    }
+
+    public void setMch_id(String mch_id) {
+        this.mch_id = mch_id;
+    }
+
+    /**
+     * 设备号
+     */
+    private String device_info;
+
+    public String getDevice_info() {
+        return device_info;
+    }
+
+    public void setDevice_info(String device_info) {
+        this.device_info = device_info;
+    }
+
+    /**
+     * 随机字符串
+     */
+    private String nonce_str;
+
+    public String getNonce_str() {
+        return nonce_str;
+    }
+
+    public void setNonce_str(String nonce_str) {
+        this.nonce_str = nonce_str;
+    }
+
+    /**
+     * 签名
+     */
+    private String sign;
+
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+
+    /**
+     * 签名类型
+     */
+    private String sign_type;
+
+    public String getSign_type() {
+        return sign_type;
+    }
+
+    public void setSign_type(String sign_type) {
+        this.sign_type = sign_type;
+    }
+
+    /**
+     * 业务结果
+     */
+    private String result_code;
+
+    public String getResult_code() {
+        return result_code;
+    }
+
+    public void setResult_code(String result_code) {
+        this.result_code = result_code;
+    }
+
+    /**
+     * 错误代码
+     */
+    private String err_code;
+
+
+    public String getErr_code() {
+        return err_code;
+    }
+
+    public void setErr_code(String err_code) {
+        this.err_code = err_code;
+    }
+
+    /**
+     * 错误代码描述
+     */
+    private String err_code_des;
+
+    public String getErr_code_des() {
+        return err_code_des;
+    }
+
+    public void setErr_code_des(String err_code_des) {
+        this.err_code_des = err_code_des;
+    }
+
+    /**
+     * 用户标识
+     */
+    private String openid;
+
+    public String getOpenid() {
+        return openid;
+    }
+
+    public void setOpenid(String openid) {
+        this.openid = openid;
+    }
+
+    /**
+     * 是否关注公众账号
+     * <p>
+     * 用户是否关注公众账号，Y-关注，N-未关注，仅在公众账号类型支付有效
+     */
+    private String is_subscribe;
+
+    public String getIs_subscribe() {
+        return is_subscribe;
+    }
+
+    public void setIs_subscribe(String is_subscribe) {
+        this.is_subscribe = is_subscribe;
+    }
+
+    /**
+     * 交易类型
+     */
+    private String trade_type;
+
+    public String getTrade_type() {
+        return trade_type;
+    }
+
+    public void setTrade_type(String trade_type) {
+        this.trade_type = trade_type;
+    }
+
+    /**
+     * 付款银行
+     */
+    private String bank_type;
+
+
+    public String getBank_type() {
+        return bank_type;
+    }
+
+    public void setBank_type(String bank_type) {
+        this.bank_type = bank_type;
+    }
+
+    /**
+     * 订单金额
+     */
+    private int total_fee;
+
+    public int getTotal_fee() {
+        return total_fee;
+    }
+
+    public void setTotal_fee(int total_fee) {
+        this.total_fee = total_fee;
+    }
+
+    /**
+     * 应结订单金额
+     */
+    private Integer settlement_total_fee;
+
+    public Integer getSettlement_total_fee() {
+        return settlement_total_fee;
+    }
+
+    public void setSettlement_total_fee(Integer settlement_total_fee) {
+        this.settlement_total_fee = settlement_total_fee;
+    }
+
+    /**
+     * 货币种类
+     */
+    private String fee_type;
+
+
+    public String getFee_type() {
+        return fee_type;
+    }
+
+    public void setFee_type(String fee_type) {
+        this.fee_type = fee_type;
+    }
+
+    /**
+     * 现金支付金额
+     */
+    private Integer cash_fee;
+
+    public Integer getCash_fee() {
+        return cash_fee;
+    }
+
+    public void setCash_fee(Integer cash_fee) {
+        this.cash_fee = cash_fee;
+    }
+
+    /**
+     * 现金支付货币类型
+     */
+    private String cash_fee_type;
+
+    public String getCash_fee_type() {
+        return cash_fee_type;
+    }
+
+    public void setCash_fee_type(String cash_fee_type) {
+        this.cash_fee_type = cash_fee_type;
+    }
+
+    /**
+     * 总代金券金额
+     */
+    private Integer coupon_fee;
+
+    public Integer getCoupon_fee() {
+        return coupon_fee;
+    }
+
+    public void setCoupon_fee(Integer coupon_fee) {
+        this.coupon_fee = coupon_fee;
+    }
+
+    /**
+     * 代金券使用数量
+     */
+    private Integer coupon_count;
+
+    public Integer getCoupon_count() {
+        return coupon_count;
+    }
+
+    public void setCoupon_count(Integer coupon_count) {
+        this.coupon_count = coupon_count;
+    }
+
+    /**
+     * 微信支付订单号
+     */
+    private String transaction_id;
+
+    public String getTransaction_id() {
+        return transaction_id;
+    }
+
+    public void setTransaction_id(String transaction_id) {
+        this.transaction_id = transaction_id;
+    }
+
+    /**
+     * 商户订单号
+     */
+    private String out_trade_no;
+
+    public String getOut_trade_no() {
+        return out_trade_no;
+    }
+
+    public void setOut_trade_no(String out_trade_no) {
+        this.out_trade_no = out_trade_no;
+    }
+
+    /**
+     * 商家数据包
+     */
+    private String attach;
+
+    public String getAttach() {
+        return attach;
+    }
+
+    public void setAttach(String attach) {
+        this.attach = attach;
+    }
+
+    /**
+     * 支付完成时间
+     */
+    private String time_end;
+
+    public String getTime_end() {
+        return time_end;
+    }
+
+    public void setTime_end(String time_end) {
+        this.time_end = time_end;
+    }
+}
